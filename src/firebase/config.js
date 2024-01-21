3f22b03d551e54f4425c095b222aab595e842cfd
Aftab Mulani <93897204+Aftab-M@users.noreply.github.com>
2024-01-21 09:39:41 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,13 @@ import {getFirestore} from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC3up_hGbU5JA5-2jdI5WM3_MO8Gv5_dbg',
-  authDomain: 'hortel-e5883.firebaseapp.com',
-  databaseURL: 'https://hortel-e5883.firebaseio.com',
-  projectId: 'hortel-e5883',
-  storageBucket: 'hortel-e5883.appspot.com',
-  messagingSenderId: '862288269596',
-  appId: '1:862288269596:android:53a50ddebbdadb2488aad9',
+  apiKey: 'api_key',
+  authDomain: 'domain',
+  databaseURL: 'url',
+  projectId: 'id',
+  storageBucket: 'bucket',
+  messagingSenderId: 'id',
+  appId: 'appid',
 };
 
 
